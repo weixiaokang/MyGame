390c27d1d97a063ec846a1683016bd9eae1cb2b8
weixiaokang <kang21472382@qq.com>
2014-08-24 23:57:19 +0800

    add a start activity

---
+++ app/src/main/java/com/weixiaokang/threepointchess/StartActivity.java
@@ -0,0 +1,52 @@
+package com.weixiaokang.threepointchess;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class StartActivity extends Activity implements Runnable{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_start);
+
+        Handler handler = new Handler();
+        handler.postDelayed(this, 3000);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.start, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void run() {
+        Intent intent = new Intent(StartActivity.this, MyActivity.class);
+        startActivity(intent);
+        StartActivity.this.finish();
+    }
+}
