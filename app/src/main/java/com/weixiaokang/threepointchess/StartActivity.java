7d9d86e7d82f654d6313cc2794e60f02c58c8160
weixiaokang <kang21472382@qq.com>
2014-08-25 00:05:51 +0800

    add a start activity

--- app/src/main/java/com/weixiaokang/threepointchess/StartActivity.java
+++ app/src/main/java/com/weixiaokang/threepointchess/StartActivity.java
@@ -15,8 +15,8 @@ public class StartActivity extends Activity implements Runnable{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+       /* requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
         setContentView(R.layout.activity_start);
 
         Handler handler = new Handler();
